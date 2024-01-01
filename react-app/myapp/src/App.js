303a745d19790751af7eb4eff5c0331b2a3d6d54
Abhisinghs <abhisheksingh35716@gmail.com>
2024-01-01 16:42:31 +0530

    understading of props and fragment tag

--- react-app/myapp/src/App.js
+++ react-app/myapp/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 
+function MyComponent({name}){
+  return (
+    <>
+      <div className={name}>{name}</div>
+    <hr/>
+    </>
+  )
+}
 function App() {
   return (
-   <div> Hello World!</div>
+  <div id="hello"> Hello World! <MyComponent name={1} /> <MyComponent name={"abhi1"}/> <MyComponent name={"abhi2"}/> </div>
   )
 }
 export default App;
